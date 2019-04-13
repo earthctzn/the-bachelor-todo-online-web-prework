56ee558bc610757195f6acb503bf66aecb018975
Caleb Einarr <earthctzn1@gmail.com>
2019-04-13 01:51:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,14 +1,8 @@
-require 'pry'
-
 def get_first_name_of_season_winner(data, season)
   winner = ""
-  #binding.pry
   data.each do |season_number, contestant_array|
-    #binding.pry
     contestant_array.each do |contestant|
-      #binding.pry
       contestant.each do |contestant_attribute, attribute_value|
-      #binding.pry
         if season == season_number && contestant_attribute == "status" && attribute_value == "Winner"
           winner = contestant["name"]
         end
@@ -21,13 +15,9 @@ end
 
 def get_contestant_name(data, occupation)
   name = ""
-  #binding.pry
   data.each do |season_number, contestant_array|
-    #binding.pry
     contestant_array.each do |contestant|
-      #binding.pry
       contestant.each do |contestant_attribute, attribute_value|
-      #binding.pry
         if contestant_attribute == "occupation" && attribute_value == occupation
           name = contestant["name"]
         end
@@ -39,13 +29,9 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   hometown_array = []
-  #binding.pry
   data.each do |season_number, contestant_array|
-    #binding.pry
     contestant_array.each do |contestant|
-      #binding.pry
       contestant.each do |contestant_attribute, attribute_value|
-      #binding.pry
         if attribute_value == hometown
           hometown_array << contestant["name"]
           #binding.pry
